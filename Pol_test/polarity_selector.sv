298bf41dad29db1b4f7dce882c37edbdd18c1760
hemakandregula-123 <hema@svtek.co.in>
2024-12-31 11:12:19 +0530

    31pol_test

---
+++ Pol_test/polarity_selector.sv
@@ -0,0 +1,15 @@
+module polarity_selector (
+    input logic [1:0] req_i,     // 2-bit input request signal (req_i)
+    output logic pol_out         // Output signal (polarity_o) representing the selected polarity
+);
+
+    // Always block, sensitivity list includes all signals used in the block
+    always_comb 
+	  begin
+        case(req_i)
+		    2'b10 : pol_out = 1'b1;
+			 2'b01 : pol_out = 1'b0;
+		    default : pol_out = 1'b0;
+        endcase
+	  end	 
+endmodule
