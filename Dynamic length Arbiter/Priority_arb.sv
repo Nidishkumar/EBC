787421cec3a77ca311810dc1f6803c80004bd55b
hemakandregula-123 <hema@svtek.co.in>
2025-01-06 13:44:33 +0530

    timestamp_update

--- Dynamic length Arbiter/Priority_arb.sv
+++ Dynamic length Arbiter/Priority_arb.sv
@@ -18,7 +18,7 @@ module Priority_arb (
   generate
     // Grant[i] is asserted if req_i[i] is active and no higher-priority grants are active
     for (i = 1; i < WIDTH; i = i + 1) 
-	   begin 
+	   begin : loop
          assign gnt_o[i] = req_i[i] & ~(|gnt_o[i-1:0]);  
      end
   endgenerate
