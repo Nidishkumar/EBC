5293898325f3858edc92a9b36fdcc8970308124a
hemakandregula-123 <hema@svtek.co.in>
2025-01-09 23:31:34 +0530

    AER_updates

--- Dynamic length Arbiter/Priority_arb.sv
+++ Dynamic length Arbiter/Priority_arb.sv
@@ -7,8 +7,8 @@
 import arbiter_pkg::*;               // Importing arbiter package containing parameter constants
 
 module Priority_arb (
-  input logic [WIDTH-1:0] req_i   ,  // Request inputs
-  output logic [WIDTH-1:0] gnt_o     // One-hot grant signal
+  input logic [ROWS-1:0] req_i   ,  // Request inputs
+  output logic [ROWS-1:0] gnt_o     // One-hot grant signal
 );
 
   // Port[0] has highest priority
@@ -17,7 +17,7 @@ module Priority_arb (
   genvar i;
   generate
     // Grant[i] is asserted if req_i[i] is active and no higher-priority grants are active
-    for (i = 1; i < WIDTH; i = i + 1) 
+    for (i = 1; i < ROWS; i = i + 1) 
 	    begin : loop
          assign gnt_o[i] = req_i[i] & ~(|gnt_o[i-1:0]);  
       end
